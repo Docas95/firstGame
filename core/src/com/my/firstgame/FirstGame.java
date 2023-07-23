f360da0398984fb8b14677c787e177cb0f4942f7
MafaldaAraujo <1220899@isep.ipp.pt>
2023-07-23 20:58:32 +0100

    Made it so that numerous balls jump around the screen instead of one

--- core/src/com/my/firstgame/FirstGame.java
+++ core/src/com/my/firstgame/FirstGame.java
@@ -5,15 +5,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.GL20;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class FirstGame extends ApplicationAdapter{
 
-	Ball bouncingBall = new Ball(50, 50, 50, 12, 5);
+	ArrayList<Ball> bouncingBalls = new ArrayList<>();
+	Random r = new Random();
 	ShapeRenderer shapeRenderer;
 
 	// runs when the program begins
 	@Override
 	public void create(){
 		this.shapeRenderer = new ShapeRenderer();
+
+		for (int i = 0; i < 10; i++) {
+			bouncingBalls.add(new Ball(r.nextInt(Gdx.graphics.getWidth()), r.nextInt(Gdx.graphics.getHeight()), r.nextInt(70), r.nextInt(20), r.nextInt(14)));
+		}
 	}
 
 	// runs every frame
@@ -22,13 +30,14 @@ public class FirstGame extends ApplicationAdapter{
 		// clear screen at the beginning of frame
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-
-		// makes the ball run back and forward between the edges of the screen while jumping
-		bouncingBall.update();
-
-		// draws a circle at x, y position, with radius radius
 		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-		bouncingBall.draw(shapeRenderer);
+		// makes the ball run back and forward between the edges of the screen while jumping
+		for(Ball ball : bouncingBalls){
+			ball.update();
+			// draws a circle at x, y position, with radius radius
+			ball.draw(shapeRenderer);
+		}
+
 		shapeRenderer.end();
 	}
 }
